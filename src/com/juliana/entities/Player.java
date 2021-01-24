8b4b3a58d7833089f835ad06e8cc7ea45c13f321
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2021-01-24 12:39:57 -0300

    adicionado animação do player caminhando para cima e para baixo

--- src/com/juliana/entities/Player.java
+++ src/com/juliana/entities/Player.java
@@ -11,25 +11,36 @@ public class Player extends Entity {
 	public double speed = 0.2; 
 	
 	
-	public int turned_right = 0 , turned_left = 1;
+	public int turned_right = 0 , turned_left = 1, turned_up = 2,  turned_down = 3;
 	public int turned = turned_right ;
 	public boolean moved = false ; 
 	
-	private int frames = 0 , maxFrames = 8 , index = 0 , maxIndex = 3 ; 
+	private int frames = 0 , maxFrames = 6 , index = 0 , maxIndex = 3 ; 
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
+	private BufferedImage[] upPlayer;
+	private BufferedImage[] downPlayer;
 	
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 		
 		rightPlayer = new BufferedImage[4];
 		leftPlayer = new BufferedImage[4];
+		upPlayer = new BufferedImage[4];
+		downPlayer = new BufferedImage[4];
+		
 		
 		for(int i = 0; i < 4  ; i++ ) {
 		rightPlayer[i] = Game.spritesheet.getSprite(32 + (i *16), 0 , 16, 16);
 		}
 		for(int i = 0; i < 4 ; i++) {
 		leftPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16), 16, 16, 16);
+		}
+		for(int i = 0; i < 4 ; i++ ) {
+			upPlayer[i] = Game.spritesheet.getSprite(32 + (i *16), 48 , 16, 16);
+		}
+		for(int i = 0; i < 4 ; i++ ) {
+			downPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16 ), 32 , 16 , 16);
 		}
 	}
 	
@@ -49,9 +60,13 @@ public class Player extends Entity {
 		}
 		
 		if(up) {
+			moved = true;
+			turned = turned_up;
 			y-=2;
 		}
 		else if(down) {
+			moved = true;
+			turned = turned_down;
 			y+=2;
 		}
 		
@@ -73,6 +88,12 @@ public class Player extends Entity {
 		}
 		else if(turned == turned_left) {
 			g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+		}
+		if(turned == turned_up) {
+			g.drawImage(upPlayer[index], this.getX(), this.getY(), null);
+		}
+		else if(turned == turned_down) {
+			g.drawImage(downPlayer[index], this.getX(), this.getY() , null);
 		}
 	}	
 		
